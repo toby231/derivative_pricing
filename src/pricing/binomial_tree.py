8ea542873536ac458e9ec89402445f44328c9f03
Toby Nguyen <tobynguyen@Tobys-MacBook-Air-3.local>
2026-08-26 21:44:00 +1000

    Defined option parameters

---
+++ src/pricing/binomial_tree.py
@@ -0,0 +1,8 @@
+import numpy
+
+from utils.option_properties import Option
+
+# Start off with one step binomial tree
+
+def onestep(Option):
+    return 
---
+++ src/utils/option_properties.py
@@ -0,0 +1,25 @@
+import numpy as np
+from dataclasses import dataclass
+
+@dataclass
+class Option:
+
+    """
+    A simple container for option parameters.
+    Use as: r, T, n, S_0, K, sigma.
+    """
+
+    # Basic parameters
+
+    S_0: float = 100.0 # Initial Stock Price
+    K: float = 100.0 # Strike Price 
+    r: float = 0.05 # Risk-free rate
+    T: float = 1.0 # Time to maturity in years
+    n: int = 10 # Number of steps
+    sigma: float = 0.05 # Volatility
+
+    def __post_init__(self):
+        dt = self.T / self.n
+
+        self.u = np.exp(self.sigma * np.sqrt(dt)) 
+        self.d = np.exp(-self.sigma * np.sqrt(dt))
